ef44c2fe681fe47f567f66f8faece52d0c420769
skotperez <voraginebramante@gmail.com>
2014-09-04 10:57:58 +0200

    basic folders and archives

---
+++ navbar-m34.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Plugin Name: Navigation bar m34
+Description: This plugin allows you to create a navigation bar for a network of federated websites.
+Version: 0.1
+Author: montera34
+Author URI: http://montera34.com
+License: GPLv2+
+*/
+
+/* EDIT THIS VARS TO CONFIG THE PLUGIN */
+$m34navbar_logo_src = plugins_url( 'images/jusdolive.navbar.png' , __FILE__);
+$m34navbar_logo_alt = "jusdolive logo";
+$m34navbar_main_url = "http://jusdolive.fr";
+/* STOP EDIT */
+
+// plugin main activation function
+//register_activation_hook( __FILE__, 'm34glossary_activate' );
+//function m34glossary_activate() {
+
+	if (!defined('M34NAVBAR_LOGO_SRC')) define('M34NAVBAR_LOGO_SRC', $m34navbar_logo_src);
+	if (!defined('M34NAVBAR_LOGO_ALT')) define('M34NAVBAR_LOGO_ALT', $m34navbar_logo_alt);
+	if (!defined('M34NAVBAR_MAIN_URL')) define('M34NAVBAR_MAIN_URL', $m34navbar_main_url);
+
+	/* Load map JavaScript and styles */
+	add_action( 'wp_enqueue_scripts', 'm34navbar_scripts_styles' );
+
+	/* Output navbar */
+	add_action( 'wp_footer', 'm34navbar_output',999 );
+	/* Output extra styles */
+	add_action( 'wp_head', 'm34navbar_extra_styles',9999 );
+//} // END plugin main activation function
+
+// Register styles and scripts
+function m34navbar_scripts_styles() {
+	wp_enqueue_style( 'm34navbar-css',plugins_url( 'style/m34navbar.css' , __FILE__) );
+} // END register scripts and styles
+
+// Output extra styles
+function m34navbar_extra_styles() {
+
+	if ( is_admin_bar_showing() ) { $mtop = "67"; $top = "32"; } else { $mtop = "35"; $top = "0"; }
+	$m34navbar_extra_style = "<style media='print' type='text/css'>#m34navbar-system{display:none;}</style>";
+	$m34navbar_extra_style .= "<style media='screen' type='text/css'>html,* html body {margin-top: " .$mtop. "px !important;}#m34navbar-system{ top: " .$top. "px;}</style>";
+	echo $m34navbar_extra_style;
+} // END output extra styles
+
+// Generate navbar output
+function m34navbar_output() {
+
+	$navbar_output = "
+	<nav id='m34navbar-system'>
+		<div class='system-limit'>
+			<div class='system-sun'>
+				<a class='system-swicht' href=''><img src='' alt='' /></a>
+				<a class='system-light' href='" .M34NAVBAR_MAIN_URL. "'><img src='" .M34NAVBAR_LOGO_SRC. "' alt='" .M34NAVBAR_LOGO_ALT. "' /></a>
+			</div><!-- .system-sun -->
+			<div class='system-planets'>
+				<ul id='planet-1' class='system-planet planet-left'>
+					<li><a href=''>About</a></li>
+					<li><a href=''>Services</a></li>
+					<li><a href=''>Contact</a></li>
+				</ul><!-- .system-planet -->
+				<ul id='planet-2' class='system-planet planet-right'>
+					<li><a href=''>About</a></li>
+					<li><a href=''>Services</a></li>
+					<li><a href=''>Contact</a></li>
+				</ul><!-- .system-planet -->
+			</div><!-- .system-planets -->
+		</div><!-- .system-limit -->
+	</nav>
+	";
+	echo $navbar_output;
+} // END generate navbar output
+
+?>
